13be9a8133874b35048aa85c31ce71a1be45f5c1
mtvalencia <mtv411@yahoo.com>
2019-07-21 22:49:49 -0600

    Modified folder structure

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
+var app = express();
+
+var router = express.Router();
+
+// Parse request body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+app.use(router);
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/iscrape", { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
